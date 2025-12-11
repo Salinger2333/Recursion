bb9f4f53a4e70e81abcbb7581c374ff28b5f6e4a
Salinger2333 <1565664814@qq.com>
2025-12-11 12:06:50 +0800

    solve merge sort problem

--- mergeSort.js
+++ mergeSort.js
@@ -1,20 +1,27 @@
 function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
   let leftArr = arr.slice(0, arr.length / 2);
   let rightArr = arr.slice(arr.length / 2);
-  let sortArr = [];
-  
 
+  function merge(leftArr, rightArr) {
+    let sortArr = [];
+    do {
+      if (leftArr[0] < rightArr[0]) {
+        sortArr.push(leftArr.shift());
+      } else {
+        sortArr.push(rightArr.shift());
+      }
+    } while (leftArr.length > 0 && rightArr.length > 0);
+    sortArr = sortArr.concat(leftArr, rightArr);
+    return sortArr;
+  }
 
-  do {
-    if (leftArr[0] < rightArr[0]) {
-      sortArr.push(leftArr.shift());
-    } else {
-      sortArr.push(rightArr.shift());
-    }
-  } while (leftArr.length > 0 && rightArr.length > 0);
-  sortArr = sortArr.concat(leftArr, rightArr);
-  return sortArr;
-  
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
-
-console.log(mergeSort([2, 1, 3, 5, 8, 10]));
+console.log(mergeSort([]));
+console.log(mergeSort([73]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
